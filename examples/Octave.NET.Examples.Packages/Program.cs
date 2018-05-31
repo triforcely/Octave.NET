0bf233e2f336d2dbbc20f8b6fe608db397fa2c9d
Michał Wilski <michal.wilski@gmail.com>
2018-06-01 01:00:01 +0200

    Version bump.

--- examples/Octave.NET.Examples.Packages/Program.cs
+++ examples/Octave.NET.Examples.Packages/Program.cs
@@ -6,9 +6,10 @@
         {
             using (var octave = new OctaveContext())
             {
-                octave.Execute("pkg load fuzzy-logic-toolkit;"); // I don't care about output from this command so semicolon is fine
-
-                var algebraicPoduct = octave.Execute("algebraic_product(5,2)").AsScalar(); // 10
+                octave.Execute("pkg load fuzzy-logic-toolkit;");
+                double result = octave
+                    .Execute("algebraic_product(5,2)")
+                    .AsScalar(); // 10
             }
         }
     }
