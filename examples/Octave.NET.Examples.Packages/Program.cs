4edf21d869ef691af275db5386cace22df96e382
Michał Wilski <michal.wilski@gmail.com>
2018-05-31 12:29:32 +0200

    Examples 📚

---
+++ examples/Octave.NET.Examples.Packages/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Octave.NET.Examples.Packages
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            using (var octave = new OctaveContext())
+            {
+                octave.Execute("pkg load fuzzy-logic-toolkit;"); // I don't care about output from this command so semicolon is fine
+
+                var algebraicPoduct = octave.Execute("algebraic_product(5,2)").AsScalar(); // 10
+            }
+        }
+    }
+}
---
+++ examples/Octave.NET.Examples/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Octave.NET.Examples.Basics
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            using (var octave = new OctaveContext())
+            {
+                // Please note that there is no ";" at the end. This is important if we want to capture the output.
+                var scalarResult = octave
+                        .Execute("2 + 2")
+                        .AsScalar();
+
+                var vectorResult = octave
+                    .Execute("[1 2 3 4 5]")
+                    .AsVector();
+
+                var matrixResult = octave
+                    .Execute("[1 2 3 4 5; 5 4 3 2 1]")
+                    .AsMatrix();
+
+                var vec = new double[] { 1, 2, 3, 4, 5 };
+                var anotherVector = octave.Execute(vec.ToOctave()); // [1 2 3 4 5]
+
+                var mat = new[]
+                {
+                    new double[] { 1, 2, 3, 4, 5 },
+                    new double[] { 1, 2, 3, 4, 5 }
+                };
+                var anotherMatrix = octave.Execute(mat.ToOctave());// [1 2 3 4 5;1 2 3 4 5]
+            }
+        }
+    }
+}
