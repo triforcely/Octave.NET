09142ca5ec73d15fa7ee92e04e6b9b24a0178f30
Michał Wilski <michal.wilski@gmail.com>
2018-05-31 12:00:36 +0200

    Extension methods for easy vector and matrix conversions to octave format.

---
+++ src/Octave.NET.Tests/OctaveDoubleExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Octave.NET.Tests
+{
+    [TestClass]
+    public class OctaveDoubleExtensionsTests
+    {
+        [TestMethod]
+        public void VectorToOctave_ReturnsCorrectResult()
+        {
+            //arrange
+            var input = new double[] { 1, 2, 3 };
+
+            //act
+            var res = input.ToOctave();
+
+            //assert
+            Assert.AreEqual(res, "[1 2 3]");
+        }
+
+        [TestMethod]
+        public void MatrixToOctave_ReturnsCorrectResult()
+        {
+            //arrange
+            var input = new[]
+            {
+                new double[] {1, 2, 3},
+                new double[] {3, 2, 1}
+            };
+
+            //act
+            var res = input.ToOctave();
+
+            //assert
+            Assert.AreEqual(res, "[1 2 3;3 2 1]");
+        }
+    }
+}
---
+++ src/Octave.NET/OctaveDoubleExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Linq;
+
+namespace Octave.NET
+{
+    public static class OctaveDoubleExtensions
+    {
+        private static string BuildVector(double[] vector)
+        {
+            return vector.Select(x => x.ToString(CultureInfo.InvariantCulture)).Aggregate((a, b) => $"{a} {b}");
+        }
+
+        private static string BuildMatrix(double[][] matrix)
+        {
+            return matrix.Select(BuildVector).Aggregate((a, b) => $"{a};{b}");
+        }
+
+        /// <summary>
+        /// Convert array of doubles to octave input.
+        /// </summary>
+        public static string ToOctave(this double[] vector)
+        {
+            return $"[{BuildVector(vector)}]";
+        }
+
+        public static string ToOctave(this double[][] matrix)
+        {
+            return $"[{BuildMatrix(matrix)}]";
+        }
+    }
+}
