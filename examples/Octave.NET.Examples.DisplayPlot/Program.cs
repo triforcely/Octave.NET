c92bf0f35ba9fe2adaf0655114799dd068a40fbb
Michał Wilski <michal.wilski@gmail.com>
2018-05-31 12:49:38 +0200

    Display plot example 💹

---
+++ examples/Octave.NET.Examples.DisplayPlot/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Octave.NET.Examples.DisplayPlot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var octave = new OctaveContext())
+            {
+                // Note: your octave-cli must support some plot backend, in case of problems investigate manually in octave-cli
+
+                var script = @"
+x = -10:0.1:10;
+y = sin (x);
+
+handle = plot (x, y);
+
+title (""Hello from C#"");
+xlabel (""x"");
+ylabel (""sin (x)"");
+
+waitfor(handle); #                 <- without that plot window would not show 
+                ";
+
+                octave.Execute(script, int.MaxValue);
+            }
+        }
+    }
+}
